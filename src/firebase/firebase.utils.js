1aa5b736fb6eb57de973e195b2a23c86d6ae931e
Monwar Khan <72820069+MonwarK@users.noreply.github.com>
2021-06-24 14:07:43 +0100

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,12 +1,7 @@
 import firebase from "firebase"
 
 const config = {
-    apiKey: "AIzaSyCBeKMia6xvVm95UHu5jM52mHFKzbMHMpg",
-    authDomain: "discord-clone-6b057.firebaseapp.com",
-    projectId: "discord-clone-6b057",
-    storageBucket: "discord-clone-6b057.appspot.com",
-    messagingSenderId: "377849015150",
-    appId: "1:377849015150:web:fbc32726be597b0fc56f7f"
+   
 }
 
 firebase.initializeApp(config)
